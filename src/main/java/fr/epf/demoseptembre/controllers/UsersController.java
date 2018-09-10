79855e107bff323e347b11e7419cc2e648660936
AmbroiseS <amb.soullier@gmail.com>
2018-09-11 01:46:49 +0200

    added ressources and POST example

--- src/main/java/fr/epf/demoseptembre/controllers/UsersController.java
+++ src/main/java/fr/epf/demoseptembre/controllers/UsersController.java
@@ -1,9 +1,11 @@
 package fr.epf.demoseptembre.controllers;
 
+import fr.epf.demoseptembre.models.User;
 import fr.epf.demoseptembre.persistence.UserDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * TODO class details.
@@ -13,25 +15,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UsersController {
 
-  private final UserDao userDao;
-  
-  public UsersController(UserDao userDao) {
-    this.userDao = userDao;
-  }
+    private final UserDao userDao;
 
-  /**
-   * Ceci sera mappé sur l'URL '/users'.
-   * C'est le routeur de Spring MVC qui va détecter et appeler directement cette méthode.
-   * Il lui fournira un "modèle", auquel on pourra rajouter des attributs.
-   * Ce modèle sera ensuite forwardé à une page web (dans resources/templates).
-   * Le nom de la template est retourné par la fonction. Ici, elle appelle donc le template "users".
-   * @param model le modèle
-   * @return
-   */
-  @GetMapping("/users")
-  public  String getUsers (Model model) {
-    model.addAttribute("data", userDao.findAll());
-    return "users-list";
-  }
-  
+    public UsersController(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    /**
+     * Ceci sera mappé sur l'URL '/users'.
+     * C'est le routeur de Spring MVC qui va détecter et appeler directement cette méthode.
+     * Il lui fournira un "modèle", auquel on pourra rajouter des attributs.
+     * Ce modèle sera ensuite forwardé à une page web (dans resources/templates).
+     * Le nom de la template est retourné par la fonction. Ici, elle appelle donc le template "users".
+     *
+     * @param model le modèle
+     * @return
+     */
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        model.addAttribute("data", userDao.findAll());
+        return "users-list";
+    }
+
+    @GetMapping("/user")
+    public String addUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "add_member";
+    }
+
+    @PostMapping("/user")
+    public String addUser(User user, Model model) {
+        userDao.save(user);
+        return "redirect:/users";
+    }
+
+
 }
